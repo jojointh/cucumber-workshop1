476a7d146334095658b63bf87eb163b942218a60
JoJo <jojo141a@hotmail.com>
2018-08-19 10:07:20 +0700

    init

---
+++ features/fizzbuzz.feature
@@ -0,0 +1,9 @@
+Feature: Fizzbuss counting system
+    Scenario: 1 is 1
+    Given I got 1
+    When I count
+    Then I should got "1"
+
+    
+
+
---
+++ features/step_definitions/fizzbuzz_steps.rb
@@ -0,0 +1,11 @@
+Given("I got {int}") do |input|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I count") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should got {string}") do |expected|
+    pending # Write code here that turns the phrase above into concrete actions
+end
