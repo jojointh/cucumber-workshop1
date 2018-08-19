4ffb1b9fd4e9a6f33307cad24d5874b91a2dade8
JoJo <jojo141a@hotmail.com>
2018-08-19 10:21:17 +0700

    Adding fizzbuzz step

--- features/step_definitions/fizzbuzz_steps.rb
+++ features/step_definitions/fizzbuzz_steps.rb
@@ -1,11 +1,18 @@
+class Fizzbuzz
+    def count(input)
+        "1"
+    end
+end
+
 Given("I got {int}") do |input|
-    pending # Write code here that turns the phrase above into concrete actions
+   @input = input
+   @fizzbuss = Fizzbuzz.new 
 end
 
 When("I count") do
-    pending # Write code here that turns the phrase above into concrete actions
+    @actual = @fizzbuss.count @input
 end
 
 Then("I should got {string}") do |expected|
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(@actual).to eq expected
 end
